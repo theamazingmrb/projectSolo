4744be2c5b0347bda87a45fb0199e7177e4dbaa4
billie heidelberg <billie.heidelberg.jr@gmail.com>
2017-04-10 16:36:16 -0700

    add delete to the controller

--- solo/js/soloController.js
+++ solo/js/soloController.js
@@ -6,6 +6,9 @@ SoloController.$inject = ['$http']
 function SoloController($http) {
   var self = this
   self.all = []
+  self.newUser = {}
+
+  self.addUser = addUser
 
   function displayUsers() {
     $https
@@ -15,4 +18,21 @@ function SoloController($http) {
       })
   }
   displayUsers()
+
+  function addUser() {
+    $http
+      .post('https://sheltered-sea-41109.herokuapp.com/api/users', self.newUser)
+      .then(function(res) {
+        displayUsers()
+      })
+      self.newUser = {}
+  }
+
+  function deleteUser() {
+    $http
+      .delete('https://sheltered-sea-41109.herokuapp.com/api/users' + user._id)
+      .then(function(res) {
+        displayUsers()
+      })
+  }
 }
